ecfe8b161d20576ee25ddbf5f16c08a4a13838e1
Hunt <71895829+AnonymousErrors@users.noreply.github.com>
2021-08-25 10:46:38 +0100

    Update speedmansim.lua

--- speedmansim.lua
+++ speedmansim.lua
@@ -1,1 +1,1 @@
- game:GetService("StarterGui"):SetCore("SendNotification", { Title = "Patched", Text = "This script was patched yesterday, join discord.gg/87yd7sx6B8 for further updates", Duration = 4 })
+ game:GetService("StarterGui"):SetCore("SendNotification", { Title = "Patched", Text = "This script was patched yesterday, join discord.gg/87yd7sx6B8 for further updates", Duration = 30 })
