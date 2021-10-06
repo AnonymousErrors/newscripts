73153bdad430e161e094eb484043efae943e14e6
Hunt <71895829+AnonymousErrors@users.noreply.github.com>
2021-10-06 08:03:16 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+local pid = game.PlaceId
+if pid == 6951762614 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/ufosim.lua"))()
+elseif pid == 7065731541 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/speedmansim.lua"))()
+elseif pid == 7025847767 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/destroyedkidsim.lua"))()
+end
