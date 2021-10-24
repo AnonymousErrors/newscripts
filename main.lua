c2718ad92cd84e32eacc28f3b352b2edbf62fdca
Hunt <71895829+AnonymousErrors@users.noreply.github.com>
2021-10-24 11:57:57 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
-_G["KEY"] = "0937EFA3-EBD9-4F8B-9C6F-A106C648F3CC"
 local pid = game.PlaceId
 if pid == 6951762614 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/ufosim.lua"))()
