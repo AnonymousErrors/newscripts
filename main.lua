0319c1d9c5291bc0b44292bb71cd4af1ebd3edf0
101BarzOn <94316211+101BarzOn@users.noreply.github.com>
2021-11-15 19:09:11 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,4 +9,6 @@ elseif pid == 7363174769 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/deliverymansim.lua"))()
 elseif pid == 6766156863 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/strongmansim.lua"))()
+elseif pid == 7336128600 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/PetLegends.lua"))()
 end
