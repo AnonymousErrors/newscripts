48537d34b6c8a5a54ef24f755a0f691b681efd83
Hunt <zuner.hunter@gmail.com>
2021-12-01 07:15:38 +0200

    Movin

--- main.lua
+++ main.lua
@@ -1,16 +1,13 @@
-local pid = game.PlaceId
-if pid == 6951762614 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/ufosim.lua"))()
-elseif pid == 7065731541 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/speedmansim.lua"))()
-elseif pid == 7025847767 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/destroyedkidsim.lua"))()
-elseif pid == 7363174769 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/deliverymansim.lua"))()
-elseif pid == 6766156863 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/strongmansim.lua"))()
-elseif pid == 7336128600 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/PetLegends.lua"))()
-elseif pid == 6769764667 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/FactorySimulator.lua"))()
+
+local bindable = Instance.new("BindableFunction")
+function bindable.OnInvoke(response)
+	print("invoked")
+	setclipboard("discord.gg/WzHYjEnUF2")
 end
+game:GetService("StarterGui"):SetCore("SendNotification", {
+	Title = "HUNT#1113 here",
+	Text = "Join discord.gg/WzHYjEnUF2 for the original script",
+	Duration = 69420,
+	Callback = bindable,
+	Button1 = "Copy"
+})
